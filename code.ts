bf17639656ca6ebf87a9ac2f6c7e72835fdf6182
Tuiuiu <lucasdario.dariolucas@gmail.com>
2020-09-09 22:02:59 -0300

    Add "random rotate" button and move rotation to a new fuction

--- code.ts
+++ code.ts
@@ -12,6 +12,7 @@ let nRows;
 let nColumns;
 let paddingSize;
 let baseWidth;
+let distanceResize = false;
 const lines: SceneNode[] = [];
 
 // Calls to "parent.postMessage" from within the HTML page will trigger this
@@ -37,10 +38,19 @@ figma.ui.onmessage = msg => {
     // Force Deviation
     // Width Contraction
     createGrid(msg);
+    rotateLines(getRandomPoint());
   }
+
+  if (msg.type === 'rotate-lines') {
+    rotateLines(getRandomPoint());
+  }
+
   // Make sure to close the plugin when you're done. Otherwise the plugin will
   // keep running, which shows the cancel button at the bottom of the screen.
-  figma.closePlugin();
+  
+  if (msg.type === 'cancel') {
+    figma.closePlugin();
+  }
 };
 
 function pointsDistance(x1, x2, y1, y2) {
@@ -52,13 +62,6 @@ function createGrid(msg) {
   nColumns = msg.columns;
   paddingSize = msg.padding; 
   baseWidth = msg.widthSize;
-
-  // Random inflection point
-  let lineRefX = Math.floor(Math.random() * nColumns);
-  let lineRefY = Math.floor(Math.random() * nRows);
-  // Random point position
-  let refX = lineRefX * paddingSize;
-  let refY = lineRefY * paddingSize;
 
   //const lines: SceneNode[] = [];
   //let elements = msg.count * msg.count;
@@ -72,66 +75,10 @@ function createGrid(msg) {
     for (let j = 0; j < nColumns; j++){
       // Create a line at the center of the padding
       const newLine = figma.createLine();
-      let centerPaddingOffset = paddingSize - baseWidth
+      let centerPaddingOffset = paddingSize - baseWidth;
       newLine.resize(baseWidth, 0);
       newLine.x = j*paddingSize + centerPaddingOffset;
       newLine.y = i*paddingSize + paddingSize/2;
-    
-      // Get distance do target point
-      let hypotenuse = pointsDistance(newLine.x, refX, newLine.y, refY);
-
-      // If it's the "inflection line"
-      if (j == lineRefX && i == lineRefY){
-        newLine.resize(0.01, 0);
-        newLine.x = newLine.x + baseWidth/2
-      }
-      // Else, it's a regular line, resize then rotate
-      else {
-        // Resize based on the distance
-
-        /*
-        let widthResize = (paddingSize/hypotenuse) < 1 ? 3*(paddingSize/hypotenuse) : 1;
-        if (widthResize > 1) {
-          widthResize = 1;
-        }
-        let newWidth = widthResize*newLine.width;
-        newLine.resize(newWidth, 0);
-        // Fix position based on the resize
-        newLine.x = newLine.x + (baseWidth - newWidth)/2;
-        */
-
-        // Referência p/ rotação: https://spectrum.chat/figma/extensions-and-api/relative-transform-point-of-rotation~7aa16373-9709-49b8-9145-5f3830ddfe32
-        // Figure out where the current line is relative to its parent
-        let locationRelativeToParentX = newLine.x;
-        let locationRelativeToParentY = newLine.y;
-    
-        // Get the center of the rotation-point based on the line size
-        let x = newLine.width/2;
-        let y = 0;
-        
-        // oc/hypotenuse = opposite cathetus / hypotenuse = sin(angle)
-        // ac/hypotenuse = adjacent cathetus / hypotenuse = cos(angle)
-        let oc = refY - locationRelativeToParentY;
-        let ac = refX - locationRelativeToParentX;
-        // Get rotation angle based on the distance to the target
-        let rotationAngle = Math.atan(oc/ac);
-        // Force Deviation reflected by Rotation "error"
-        rotationAngle += 75 * Math.PI / 180;
-        
-        // Transforms to fix line position because the rotation is done around line's starting point, and not its center point
-        let myTransformX = x - x * Math.cos(rotationAngle) + y * Math.sin(rotationAngle);
-        let myTransformY = y - x * Math.sin(rotationAngle) - y * Math.cos(rotationAngle);
-  
-        // This is the final Transform matrix that'll do the translation + rotation
-        // The final transform is [[cos(angle), sin(angle), translationX][-sin(angle), cos(angle), translationY]]
-        // The angle depicted in the last comment is the rotation angle
-        let myRotationTransform = [[Math.cos(rotationAngle), -Math.sin(rotationAngle), myTransformX],[Math.sin(rotationAngle), Math.cos(rotationAngle), myTransformY]] as Transform;
-        newLine.relativeTransform = myRotationTransform;
-        
-        // Move the line back to where it was initially relative to it's parent element
-        newLine.x = newLine.x + locationRelativeToParentX;
-        newLine.y = newLine.y + locationRelativeToParentY;    
-      }
       // Add line to document and to array
       figma.currentPage.appendChild(newLine);
       lines.push(newLine);
@@ -139,4 +86,79 @@ function createGrid(msg) {
   }
   figma.currentPage.selection = lines;
   figma.viewport.scrollAndZoomIntoView(lines);
+}
+
+function rotateLines(targetPoint) {
+  // Get targer point position in grid
+  let targetInArray = targetPoint[0] * nColumns + targetPoint[1];
+  let targetPosX = targetPoint[1]*paddingSize;
+  let targetPosY = targetPoint[0]*paddingSize;
+  for (let i = 0; i < lines.length; i++) {
+    // Reset line rotation, width and position
+    lines[i].rotation = 0;
+    lines[i].resize(baseWidth, 0);
+    let centerPaddingOffset = paddingSize - baseWidth;
+    lines[i].x = (i%nColumns)*paddingSize + centerPaddingOffset;
+    lines[i].y = (Math.floor(i/nColumns))*paddingSize + paddingSize/2;
+    // Deal with line that is a target point
+    if (i == targetInArray) {
+      lines[i].resize(0.01, 0);
+      lines[i].x = lines[i].x + baseWidth/2;
+    }
+    // Deal with the other lines
+    else {
+      let hypotenuse = pointsDistance(lines[i].x, targetPosX, lines[i].y, targetPosY);
+      
+      if (distanceResize == true) {
+        // Resize based on the distance
+        let widthResize = (paddingSize/hypotenuse) < 1 ? 3*(paddingSize/hypotenuse) : 1;
+        if (widthResize > 1) {
+          widthResize = 1;
+        }
+        let newWidth = widthResize*lines[i].width;
+        lines[i].resize(newWidth, 0);
+        // Fix position based on the resize
+        lines[i].x = lines[i].x + (baseWidth - newWidth)/2;
+      }
+
+      // Referência p/ rotação: https://spectrum.chat/figma/extensions-and-api/relative-transform-point-of-rotation~7aa16373-9709-49b8-9145-5f3830ddfe32
+      // Figure out where the current line is relative to its parent
+      let locationRelativeToParentX = lines[i].x;
+      let locationRelativeToParentY = lines[i].y;
+  
+      // Get the center of the rotation-point based on the line size
+      let x = lines[i].width/2;
+      let y = 0;
+      
+      // oc/hypotenuse = opposite cathetus / hypotenuse = sin(angle)
+      // ac/hypotenuse = adjacent cathetus / hypotenuse = cos(angle)
+      let oc = targetPosY - locationRelativeToParentY;
+      let ac = targetPosX - locationRelativeToParentX;
+      // Get rotation angle based on the distance to the target
+      let rotationAngle = Math.atan(oc/ac);
+      // Force Deviation reflected by Rotation "error"
+      rotationAngle += 75 * Math.PI / 180;
+      
+      // Transforms to fix line position because the rotation is done around line's starting point, and not its center point
+      let myTransformX = x - x * Math.cos(rotationAngle) + y * Math.sin(rotationAngle);
+      let myTransformY = y - x * Math.sin(rotationAngle) - y * Math.cos(rotationAngle);
+
+      // This is the final Transform matrix that'll do the translation + rotation
+      // The final transform is [[cos(angle), sin(angle), translationX][-sin(angle), cos(angle), translationY]]
+      // The angle depicted in the last comment is the rotation angle
+      let myRotationTransform = [[Math.cos(rotationAngle), -Math.sin(rotationAngle), myTransformX],[Math.sin(rotationAngle), Math.cos(rotationAngle), myTransformY]] as Transform;
+      lines[i].relativeTransform = myRotationTransform;
+      
+      // Move the line back to where it was initially relative to it's parent element
+      lines[i].x = lines[i].x + locationRelativeToParentX;
+      lines[i].y = lines[i].y + locationRelativeToParentY;  
+    }
+  }
+}
+
+// Return random inflection point
+function getRandomPoint() {
+  let lineRefX = Math.floor(Math.random() * nRows);
+  let lineRefY = Math.floor(Math.random() * nColumns);
+  return [lineRefX, lineRefY];
 }
